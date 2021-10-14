fc763ffc6e99187a5beda663899bd8ba1215965e
mel0620 <cunetarommel0620@gmail.com>
2021-10-14 16:10:19 +0800

    router back

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <section v-if="post">
-      <nav class="mb-8" aria-label="go back">
+      <nav style="max-width: 64rem; margin: auto auto 2rem" aria-label="go back">
         <router-back class="block" />
       </nav>
 
@@ -9,7 +9,9 @@
         <h5
           v-if="post.createdAt"
           class="inline-block py-1 px-2 my-2 bg-gray text-white text-sm font-medium rounded-sm whitespace-no-wrap"
-        >{{ formatDate(post.createdAt) }}</h5>
+        >
+          {{ formatDate(post.createdAt) }}
+        </h5>
         <h1 class="">{{ post.title }}</h1>
         <p class="mt-1 mb-4 text-primary-600 dark:text-primary-400">{{ post.description }}</p>
         <nuxt-content :document="post" />
@@ -21,19 +23,19 @@
 <script>
 export default {
   async asyncData({ $content, params, error }) {
-    let post;
+    let post
     try {
-      post = await $content("blog", params.blog).fetch();
+      post = await $content('blog', params.blog).fetch()
     } catch (e) {
-      error({ message: "Post not found" });
+      error({ message: 'Post not found' })
     }
-    return { post };
+    return { post }
   },
   methods: {
     formatDate(dateString) {
       const date = new Date(dateString)
       return date.toLocaleDateString(process.env.lang) || ''
-    }
-  }
+    },
+  },
 }
 </script>
